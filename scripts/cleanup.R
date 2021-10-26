586c7f66b40f43eb0446aa951b2607cee1529b29
Abhishek Bhatia <abhishekbhatia08@gmail.com>
2021-10-26 08:44:34 -0400

    ✨ Updated cleanup.R for Pop data

--- scripts/cleanup.R
+++ scripts/cleanup.R
@@ -118,3 +118,88 @@ yelp_tidy <- yelp_tidy[c("state",
 write_csv(yelp_tidy,
           here::here("derived_data", "yelp_tidy.csv"))
 
+# 2. POPULATION DATA ----
+## Note that the code to clean and reshape this data is based on mkiang's repo:
+## https://github.com/mkiang/county_preparedness/blob/master/code/01_get_population_data.R
+
+## Download and reshape the 2018 NCHS bridged race population file.
+
+utils::download.file(url = "https://www.cdc.gov/nchs/nvss/bridged_race/pcen_v2018_y18.txt.zip",
+                     destfile = here::here(
+                       data_path,
+                       "source_data",
+                       basename(
+                         "https://www.cdc.gov/nchs/nvss/bridged_race/pcen_v2018_y18.txt.zip"
+                       )
+                     ))
+
+
+orig_pop_df <-
+  readr::read_fwf(here::here(
+    data_path,
+    "source_data",
+    basename(
+      "https://www.cdc.gov/nchs/nvss/bridged_race/pcen_v2018_y18.txt.zip"
+    )
+  ),
+  readr::fwf_widths(
+    c(4, 4, 1, 5, 2, 1, 1, 8),
+    c(
+      "series",
+      "year",
+      "month",
+      "fips",
+      "age",
+      "racesex",
+      "hispanic",
+      "pop"
+    )
+  ))  %>%
+  dplyr::filter(year == 2018, month == 7)
+
+pop_df <- orig_pop_df %>%
+  dplyr::select(age, fips, pop) %>%
+  dplyr::mutate(age = (cut(
+    age,
+    c(0, seq(5, 85, 5), Inf),
+    include.lowest = TRUE,
+    right = FALSE,
+    labels = FALSE
+  ) - 1) * 5) %>%
+  dplyr::group_by(fips, age) %>%
+  dplyr::summarize(pop = sum(pop)) %>%
+  dplyr::ungroup()
+
+
+readr::write_csv(pop_df,
+                 here::here("derived_data", "population_by_age.csv"))
+
+## Get percentage of non-NHW population ----
+nhw_df <- orig_pop_df %>%
+  dplyr::select(age, fips, pop, racesex, hispanic) %>%
+  dplyr::filter(racesex %in% 1:2, hispanic == 1) %>%
+  dplyr::mutate(age = (cut(
+    age,
+    c(0, seq(5, 85, 5), Inf),
+    include.lowest = TRUE,
+    right = FALSE,
+    labels = FALSE
+  ) - 1) * 5) %>%
+  dplyr::group_by(fips, age) %>%
+  dplyr::summarize(nhw_pop = sum(pop)) %>%
+  dplyr::ungroup()
+
+non_white_perc <- pop_df %>%
+  dplyr::left_join(nhw_df) %>%
+  dplyr::group_by(fips) %>%
+  dplyr::summarize(pop = sum(pop),
+                   nhw_pop = sum(nhw_pop)) %>%
+  dplyr::transmute(
+    fips = fips,
+    p_nonwhite = (pop - nhw_pop) / pop * 100,
+    p_white = nhw_pop / pop * 100
+  )
+
+readr::write_csv(non_white_perc,
+                 here::here("derived_data", "percent_nonwhite_pop.csv"))
+
