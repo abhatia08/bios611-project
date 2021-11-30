8c3ea2863f217ed7458a0e62684d658855d192fc
Abhishek Bhatia <abhishekbhatia08@gmail.com>
2021-11-30 17:21:45 -0500

    🔥 Removed analytic_df code

--- scripts/05_clean_merged.R
+++ scripts/05_clean_merged.R
@@ -14,7 +14,7 @@ library(tidycensus)
 library(labelled)
 
 ## 2. Declare `here`
-here::i_am("scripts/cleanup.R")
+here::i_am("scripts/05_clean_merged.R")
 
 ## 2. Run Util.R ----
 source(here::here("scripts", "util.R"))
@@ -25,8 +25,8 @@ ensure_directory(here::here("derived_data"))
 # A. IMPORT DATA ----
 
 ## 1. Yelp Data ----
-yelp_tidy <-
-  read_csv(here::here("derived_data", "yelp_geocoded.csv"))
+yelp_df <-
+  read_csv(here::here("derived_data", "yelp_tidy.csv"))
 
 ## 2. Population (n) Data ----
 pop_df <-
@@ -45,56 +45,13 @@ api_df <-
   read_csv(here::here("derived_data", "api.csv"))
 
 # B. JOIN DATA ----
-## NOTE: Treating the Yelp Dataset as the primary dataset gives us 30 counties with complete data (analytic_df)
-## Alternatively, the plotting df will be joined using the API data first, such that all the counties are present with complete data other than Yelp data (plotting_df)
+## The plotting df will be joined using the API data first, such that all the counties are present with complete data other than Yelp data (plotting_df)
 
-## 1. Analytic data (30 counties) ----
-
-### 1.1. Setting up the base df ----
-
-analytic_df <- yelp_tidy
-
-### 1.2. Population data ----
-
-pop_wide <- pop_df %>%
-  tidyr::spread(age, pop, sep = "")
-
-analytic_df <- analytic_df %>% dplyr::left_join(pop_wide)
-
-
-analytic_df <- analytic_df %>%
-  dplyr::mutate(
-    n_pop_2018 = age0 + age5 + age10 + age15 + age20 +
-      age25 + age30 + age35 + age40 + age45 + age50 +
-      age55 + age60 + age65 + age70 + age75 + age80 +
-      age85
-  ) %>% dplyr::mutate(p65older = ((age65 + age70 + age75 + age80 +
-                                     age85) * 100 / n_pop_2018))
-
-### 1.3. Non-white population data ----
-
-analytic_df <- analytic_df %>%
-  dplyr::left_join(nonwhite_df)
-
-### 1.4. AHRF data ----
-
-analytic_df <- analytic_df %>%
-  dplyr::left_join(ahrf_subset) %>%
-  dplyr::mutate(p_business_permil = ((n_business * 1000000) / n_pop_2018))
-
-### 1.5. API data ----
-
-analytic_df <- analytic_df %>%
-  dplyr::left_join(api_df)
-
-
-## 2. Plotting Data (All counties) ----
-
-### 2.1. Setting up the base df ----
+## 1. Setting up the base df ----
 
 plotting_df <- api_df
 
-### 2.2. Population data ----
+## 2. Population data ----
 
 pop_wide <- pop_df %>%
   tidyr::spread(age, pop, sep = "")
@@ -110,60 +67,26 @@ plotting_df <- plotting_df %>%
   ) %>% dplyr::mutate(p65older = ((age65 + age70 + age75 + age80 +
                                      age85) * 100 / n_pop_2018))
 
-### 2.3. Non-white population data ----
+## 3. Non-white population data ----
 
 plotting_df <- plotting_df %>%
   dplyr::left_join(nonwhite_df)
 
-### 2.4. AHRF data ----
+## 4. AHRF data ----
 
 plotting_df <- plotting_df %>%
-  dplyr::left_join(ahrf_subset)
+  dplyr::left_join(ahrf_df)
 
-### 2.5. GINI coef ----
-
-plotting_df <- plotting_df %>%
-  dplyr::left_join(gini_df)
-
-
-### 2.6. Yelp data ----
+## 5. Yelp data ----
 
 plotting_df <- plotting_df %>%
-  dplyr::left_join(yelp_tidy) %>%
+  dplyr::left_join(yelp_df) %>%
   dplyr::mutate(p_business_permil = ((n_business * 1000000) / n_pop_2018))
 
-# G. FINAL DATASET WRANGLING ----
+# C. FINAL DATASET WRANGLING ----
 
-## 1. Analytic DF ----
-### 1.1 Subset ----
-analytic_df <- analytic_df %>% select(
-  -c(
-    name,
-    age0,
-    age5,
-    age10,
-    age15,
-    age20,
-    age25,
-    age30,
-    age35,
-    age40,
-    age45,
-    age50,
-    age55,
-    age60,
-    age65,
-    age70,
-    age75,
-    age80,
-    age85,
-    p_white,
-  )
-)
-
-## 2. Plotting DF ----
-#### 1.1 Fill County-level NAs ----
-#### (for completeness/future-proof)
+## 1 Fill County-level NAs ----
+## (for completeness/future-proof)
 
 data(fips_codes)
 fips_codes$fips <-
@@ -174,7 +97,7 @@ plotting_df <-
             plotting_df,
             by = c("fips" = "fips"))
 
-#### 1.2 Subset and rename vars ----
+##2 Subset and rename vars ----
 plotting_df <- plotting_df[c(
   "state.x",
   "state_name.x",
@@ -185,7 +108,6 @@ plotting_df <- plotting_df[c(
   "p_nonwhite",
   "p_poverty",
   "p_business_permil",
-  "gini_coef",
   "n_pop_2018"
 )]
 
@@ -195,11 +117,7 @@ plotting_df <- plotting_df %>%
                  "county" = "county.x")
 
 
-# H. WRITE DATASETS TO DIRECTORY ----
-
-readr::write_csv(analytic_df,
-                 here::here("derived_data", "analytic_data.csv"))
-
+# D. WRITE DATASET TO DIRECTORY ----
 
 readr::write_csv(plotting_df,
                  here::here("derived_data", "plotting_data.csv"))
